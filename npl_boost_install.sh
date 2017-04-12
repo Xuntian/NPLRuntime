21107c66fdc7ab7e909843d8009f673449f50cd9
LiZhiqiang <li.zq@foxmail.com>
2017-04-12 11:30:55 +0800

    Update and rename npl_install.sh to npl_boost_install.sh

--- npl_boost_install.sh
+++ npl_boost_install.sh
@@ -18,6 +18,7 @@ mkdir ./bin -p
 pushd ./bin
 wget http://sourceforge.net/projects/boost/files/boost/1.61.0/boost_1_61_0.tar.bz2 --no-check-certificate
 tar --bzip2 -xf boost_1_61_0.tar.bz2
+rm boost_1_61_0.tar.bz2
 cd boost_1_61_0
 ./bootstrap.sh --prefix=/usr/local --with-libraries="thread,date_time,filesystem,system,chrono,signals,serialization,iostreams,regex"
 ./b2 link=static threading=multi variant=release
